e7b47fdd9d316fa75b790f8a04a6f16929a87cd3
GEROM STUDIO <131904531+GEROMSTUDIO@users.noreply.github.com>
2024-03-13 14:32:00 +0100

    Update Code émetteur.ino

--- Code/Code émetteur.ino
+++ Code/Code émetteur.ino
@@ -11,9 +11,10 @@ CRGB leds[NUM_LEDS]; // Déclaration d'un tableau de LED
 
 #define pinCE   10
 #define pinCSN  9
+#define tunnel  "PIPE1"        // Nom du tunnel de communication
 
 RF24 radio(pinCE, pinCSN);
-const byte adresse[6] = "00001";
+const byte adresse[6] = tunnel;                // Adresse du tunnel
 byte valeursEnvoyees1[32]; // Canaux 1-31
 byte valeursEnvoyees2[32]; // Canaux 32-62
 byte valeursEnvoyees3[32]; // Canaux 63-93
@@ -53,7 +54,14 @@ void loop() {
       radio.setChannel(channelMap[mode - 1]); // Changer le canal RF en fonction du mode
     }
   }
-  
+
+    // Vérifier si aucun signal DMX n'a été reçu depuis plus de 3000 ms
+  if (DMXSerial.noDataSince() > 3000) {
+    // Clignoter la LED dans la couleur du mode actuel
+    blinkLED();
+   } else {
+   setModeConfiguration();
+  // Sinon, continuer à recevoir et à envoyer des données DMX
   memset(valeursEnvoyees1, 0, sizeof(valeursEnvoyees1));
   memset(valeursEnvoyees2, 0, sizeof(valeursEnvoyees2));
   memset(valeursEnvoyees3, 0, sizeof(valeursEnvoyees3));
@@ -76,18 +84,55 @@ void loop() {
     valeursEnvoyees5[i] = DMXSerial.read(i + 124);
     valeursEnvoyees6[i] = DMXSerial.read(i + 155);
   }
+   }
 
   radio.write(&valeursEnvoyees1, sizeof(valeursEnvoyees1));
   delay(1); // Un petit délai pour éviter l'interférence
   radio.write(&valeursEnvoyees2, sizeof(valeursEnvoyees2));
   delay(1);
   radio.write(&valeursEnvoyees3, sizeof(valeursEnvoyees3));
-  delay(1);
+   delay(1);
   radio.write(&valeursEnvoyees4, sizeof(valeursEnvoyees4));
   delay(1);
   radio.write(&valeursEnvoyees5, sizeof(valeursEnvoyees5));
   delay(1);
   radio.write(&valeursEnvoyees6, sizeof(valeursEnvoyees6));
+}
+
+// Fonction pour faire clignoter la LED avec la couleur du mode actuel
+void blinkLED() {
+  static unsigned long previousMillis = 0;
+  static bool ledState = false;
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= 500) { // Clignotement toutes les 500 millisecondes
+    previousMillis = currentMillis;
+    if (ledState) {
+      leds[0] = CRGB::Black; // Éteindre la LED
+    } else {
+      // Utiliser la couleur du mode actuel pour le clignotement
+      if (mode == 1) {
+        leds[0] = CRGB::Red;
+      } else if (mode == 2) {
+        leds[0] = CRGB::Green;
+      } else if (mode == 3) {
+        leds[0] = CRGB::Blue;
+      } else if (mode == 4) {
+        leds[0] = CRGB::Yellow;
+      } else if (mode == 5) {
+        leds[0] = CRGB::Purple;
+      } else if (mode == 6) {
+        leds[0] = CRGB::Cyan;
+      } else if (mode == 7) {
+        leds[0] = CRGB::White;
+      } else if (mode == 8) {
+        leds[0] = CRGB::OrangeRed; // Utiliser une nuance plus rouge pour le mode 8
+      }
+    }
+    FastLED.show();
+    ledState = !ledState;
+  }
 }
 
 void setModeConfiguration() {
